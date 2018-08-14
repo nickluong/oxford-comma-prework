c7bdb308c9860fa6e71e2c61960bb18dbfa9dcab
Nicholas  Luong <nckluong@gmail.com>
2018-08-14 01:39:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif(array.size == 2)
     array.join(" and ")
   else 
+    str = ""
     count = 0
     until count == array.size - 1  
       str << "#{array[count]}, "
