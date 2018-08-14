50aef8c530f8911fc8744c96410954928ac397df
Nicholas  Luong <nckluong@gmail.com>
2018-08-14 01:39:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if(array.size == 1)
+    array.join
+  elsif(array.size == 2)
+    array.join(" and ")
+  else 
+    str = ""
+    count = 0
+    until count == array.size - 1  
+      str << "#{array[count]}, "
+      count += 1 
+    end 
+    str << "and #{array[count]}"
+  end 
 end
